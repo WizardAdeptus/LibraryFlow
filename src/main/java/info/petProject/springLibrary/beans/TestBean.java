045b65bc641a82710c41ae2827846d9e84d92d5c
WizardAdeptus <wizard.adeptus@ya.ru>
2017-05-08 19:52:46 +0400

    creating a session

---
+++ src/main/java/info/petProject/springLibrary/beans/TestBean.java
@@ -0,0 +1,16 @@
+package info.petProject.springLibrary.beans;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestBean {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public void test(){
+        System.out.println("sessionFactory = " + sessionFactory);
+    }
+}
